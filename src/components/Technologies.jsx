594333aee3fc5e347e05d133e384556da2f0d096
AashishBishokarma <aashishbishokarma0@gmail.com>
2025-03-29 12:13:38 +0530

    added technologies section

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import { BiLogoPostgresql } from 'react-icons/bi'
+import { FaNodeJs } from 'react-icons/fa'
+import { RiReactjsLine, RiTailwindCssFill } from 'react-icons/ri'
+import { SiExpress, SiMongodb } from 'react-icons/si'
 
 const Technologies = () => {
   return (
-    <div>Technologies</div>
+    <div className='pb-24'>
+      <h2 className='my-20 text-center text-4xl'>Technologies</h2>
+      <div className='flex flex-wrap items-center justify-center gap-4'>
+        <div>
+          <RiReactjsLine className='text-7xl text-blue-400' />
+        </div>
+        <div className='p-4'>
+          <SiMongodb className='text-7xl text-green-600'/>
+        </div>
+        <div className='p-4'>
+          <FaNodeJs className='text-7xl text-green-600'/>
+        </div>
+        <div className='p-4'>
+          <SiExpress className='text-7xl'/>
+        </div>
+        <div className='p-4'>
+          <RiTailwindCssFill className='text-7xl text-blue-400'/>
+        </div>
+        <div className='p-4'>
+          <BiLogoPostgresql className='text-7xl text-sky-700'/>
+        </div>
+      </div>
+    </div>
   )
 }
 
